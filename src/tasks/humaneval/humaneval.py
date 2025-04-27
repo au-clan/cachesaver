28574fb06270167f2fbdadf0aaac85bd57e956c6
Oskar <oskar@roegild.dk>
2025-04-27 16:48:59 +0200

    humanEval dataset and start state

---
+++ src/tasks/humaneval/humaneval.py
@@ -0,0 +1,85 @@
+﻿import random
+from typing import Tuple
+
+import pandas as pd
+
+from .state import StateHumanEval
+from ...typedefs import Benchmark
+
+
+class BenchmarkStateHumanEval(Benchmark):
+    def __init__(self, path: str, split: str = "mini"):
+        """
+        Initializes the benchmark with the dataset.
+
+        Args:
+            data_path (str): Path to the dataset.
+            split (str): Name of the dataset split (e.g., "mini", "train", "validation", "test").
+        """
+
+        df = pd.read_json(path, lines=True,
+                          compression='gzip')
+        # df = pd.read_csv(path, usecols=["question", "answer"], compression="gzip")
+        df.reset_index(inplace=True)
+        data = list(
+            zip(df['index'], df['task_id'], df['prompt'], df['entry_point'], df['canonical_solution'], df['test']))
+
+        # # Compute the idxs for each subset
+        valid_idxs = set(range(len(data)))
+        #
+
+        random.seed(0)
+        mini_set_idxs = random.sample(list(valid_idxs), 10)
+        valid_idxs = valid_idxs - set(mini_set_idxs)
+
+        train_set_idxs = random.sample(list(valid_idxs), 50)
+        valid_idxs = valid_idxs - set(train_set_idxs)
+
+        validation_set_idxs = random.sample(list(valid_idxs), 50)
+        valid_idxs = valid_idxs - set(validation_set_idxs)
+
+        if split == "mini":
+            self.data = data[:10]
+        if split == "single":
+            self.data = data[:1]
+        elif split == "train":
+            self.data = data[:10]
+        elif split == "validation":
+            self.data = data[:10]
+        elif split == "test":
+            self.data = data[:10]
+        else:
+            raise ValueError("Invalid set name")
+
+    def __len__(self) -> int:
+        """
+        Returns the length of the benchmark dataset.
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx) -> Tuple[int, StateHumanEval]:
+        """
+        Returns the index and the state for the given index.
+
+        Args:
+            idx (int): Index of the data point.
+
+        Returns:
+            Tuple[int, StateHumanEval]: Index and the corresponding state.
+        """
+        index = self.data[idx][0]
+        task_id = self.data[idx][1]
+        prompt = self.data[idx][2]
+        entry_point = self.data[idx][3]
+        canonical_solution = self.data[idx][4]
+        test = self.data[idx][5]
+
+        # Create a state object
+
+        state = StateHumanEval(
+            prompt=prompt,
+            canonical_solution=canonical_solution,
+            entry_point=entry_point,
+            test=test,
+        )
+        return task_id, state
---
+++ src/tasks/humaneval/state.py
@@ -0,0 +1,36 @@
+﻿from dataclasses import dataclass
+
+from ...typedefs import State
+
+
+@dataclass(frozen=True)
+class StateHumanEval(State):
+    prompt: str
+    canonical_solution: str
+    entry_point: str
+    test: str
+
+    def serialize(self) -> dict:
+        """
+        Returns a dictionary representation of the state.
+        """
+        pass
+
+    def clone(self, randomness: int = None) -> "StateHumanEval":
+        """
+        Returns a new instance of StateHotpotQA with an optional new randomness value.
+        """
+        pass
+
+    def get_seed(self) -> int:
+        """
+        Returns the randomness value associated with the state.
+        """
+        pass
+        return self.randomness
+
+    def __hash__(self) -> int:
+        """
+        Returns a hash of the current state.
+        """
+        return hash(str(self.serialize()))
