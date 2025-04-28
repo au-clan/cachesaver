b1523054d38dc9d275284c2315b91a8dfb319b12
Oskar <65017419+OskarRVestergaard@users.noreply.github.com>
2025-04-28 20:31:08 +0200

    humanEval dataset and start state (#6)
    dataset from: https://github.com/openai/human-eval/tree/master

---
+++ src/tasks/humaneval/state.py
@@ -0,0 +1,46 @@
+﻿from dataclasses import dataclass
+
+from ...typedefs import State
+
+
+@dataclass(frozen=True)
+class StateHumanEval(State):
+    prompt: str
+    canonical_solution: str
+    entry_point: str
+    test: str
+
+    def serialize(self) -> dict:
+        """
+        Returns a dictionary representation of the state.
+        """
+        return {
+            "prompt": self.prompt,
+            "canonical_solution": self.canonical_solution,
+            "entry_point": self.entry_point,
+            "test": self.test
+        }
+
+    def clone(self, randomness: int = None) -> "StateHumanEval":
+        """
+        Returns a new instance of StateHotpotQA with an optional new randomness value.
+        """
+        return StateHumanEval(
+            prompt=self.prompt,
+            canonical_solution=self.canonical_solution,
+            entry_point=self.entry_point,
+            test=self.test,
+        )
+
+    def get_seed(self) -> int:
+        """
+        Returns the randomness value associated with the state.
+        """
+        raise NotImplementedError()
+
+
+    def __hash__(self) -> int:
+        """
+        Returns a hash of the current state.
+        """
+        return hash(str(self.serialize()))
