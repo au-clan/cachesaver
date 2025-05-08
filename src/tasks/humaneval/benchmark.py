78af229c62763a8886b33041f7d6facde2be1c31
Tereza Ehnova <73232529+tereehn@users.noreply.github.com>
2025-05-08 22:48:20 +0200

    Update benchmark.py

--- src/tasks/humaneval/benchmark.py
+++ src/tasks/humaneval/benchmark.py
@@ -1,86 +1,42 @@
-﻿import random
+import pandas as pd
 from typing import Tuple
 
-import pandas as pd
-
-from .state import StateHumanEval
+from .state import StateGame24
 from ...typedefs import Benchmark
 
-
-class BenchmarkHumanEval(Benchmark):
+class BenchmarkGame24(Benchmark):
     def __init__(self, path: str, split: str = "mini"):
-        """
-        Initializes the benchmark with the dataset.
 
-        Args:
-            data_path (str): Path to the dataset.
-            split (str): Name of the dataset split (e.g., "mini", "train", "validation", "test").
-        """
-
-        df = pd.read_json(path, lines=True,
-                          compression='gzip')
+        df = pd.read_csv(path, usecols=["Puzzles"], compression="gzip")
         df.reset_index(inplace=True)
-        data = list(
-            zip(df['index'], df['task_id'], df['prompt'], df['entry_point'], df['canonical_solution'], df['test']))
+        data = list(zip(df['index'], df['Puzzles']))
 
-        # Compute the idxs for each subset
-        valid_idxs = set(range(len(data)))
-
-        random.seed(0)
-        mini_set_idxs = random.sample(list(valid_idxs), 10)
-        valid_idxs = valid_idxs - set(mini_set_idxs)
-
-        train_set_idxs = random.sample(list(valid_idxs), 50)
-        valid_idxs = valid_idxs - set(train_set_idxs)
-
-        validation_set_idxs = random.sample(list(valid_idxs), 50)
-        valid_idxs = valid_idxs - set(validation_set_idxs)
-
-        test_set_idxs = random.sample(list(valid_idxs), 50)
-        valid_idxs = valid_idxs - set(validation_set_idxs)
-
-        if split == "single":
-            self.data = data[:1]
-        if split == "mini":
-            self.data = [data[i] for i in mini_set_idxs]
+        if  split == "mini":
+            self.data = data[:10]
         elif split == "train":
-            self.data = [data[i] for i in train_set_idxs]
+            self.data = data[850:875] + data[1025:1050]
         elif split == "validation":
-            self.data = [data[i] for i in validation_set_idxs]
+            self.data = data[875:900] + data[1000:1025]
         elif split == "test":
-            self.data = [data[i] for i in test_set_idxs]
+            self.data = data[900:1000]
         else:
             raise ValueError("Invalid set name")
 
     def __len__(self) -> int:
-        """
-        Returns the length of the benchmark dataset.
-        """
         return len(self.data)
-
-    def __getitem__(self, idx) -> Tuple[int, StateHumanEval]:
-        """
-        Returns the index and the state for the given index.
-
-        Args:
-            idx (int): Index of the data point.
-
-        Returns:
-            Tuple[int, StateHumanEval]: Index and the corresponding state.
-        """
+    
+    def __getitem__(self, idx) -> Tuple[int, StateGame24]:
         index = self.data[idx][0]
-        task_id = self.data[idx][1]
-        prompt = self.data[idx][2]
-        entry_point = self.data[idx][3]
-        canonical_solution = self.data[idx][4]
-        test = self.data[idx][5]
+        x = self.data[idx][1]
 
         # Create a state object
-
-        state = StateHumanEval(
-            prompt=prompt,
-            canonical_solution=canonical_solution,
-            entry_point=entry_point,
-            test=test,
+        # Note: Left None for randomness, which enforces a state.clone() call in the algorithm
+        state = StateGame24(
+            puzzle=x,
+            current_state=x,
+            steps=[],
+            randomness=None
         )
         return index, state
+    
+
