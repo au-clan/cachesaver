a7fa2567701d3e2aaf1a508094410c308b0b2f27
Potamitisn <nearchosp@outlook.com>
2025-09-01 21:41:25 +0200

    Adding MethodFactory

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+from typing import TypedDict
+from .typedefs import DecodingParameters
+
 class BenchmarkFactory:
     registry = {}
 
@@ -45,19 +48,68 @@ class AgentFactory:
             return cls.registry[key](*args, **kwargs)
         except KeyError:
             raise ValueError(f"No agent found for type={agent_type}, benchmark={benchmark}")
-        
+
+class AgentDictFactory:
+    registry = {}
+
+    @classmethod
+    def register(cls, agent_dict_cls):
+        cls.registry[agent_dict_cls.__name__.lower()] = agent_dict_cls
+        return agent_dict_cls
+
+    @classmethod
+    def get(cls, method: str, *args, **kwargs):
+        key = f"agentdict{method}".lower()
+        try:
+            return cls.registry[key](*args, **kwargs)
+        except KeyError:
+            raise ValueError(f"No agent dict found for method={method}")
+
+
 class MethodFactory:
     registry = {}
 
     @classmethod
-    def register(cls, algo_cls):
-        cls.registry[algo_cls.__name__.lower()] = algo_cls
-        return algo_cls
+    def register(cls, method_cls):
+        cls.registry[method_cls.__name__.lower()] = method_cls
+        return method_cls
 
     @classmethod
-    def get(cls, method: str, *args, **kwargs):
+    def get(cls, method: str, benchmark: str, params: DecodingParameters, *args, **kwargs):
         key = f"method{method}".lower()
+
+        if method == "foa":
+            agents = {
+                "step": AgentFactory.get("act", benchmark),
+                "evaluate": AgentFactory.get("evaluate", benchmark),
+            }
+        elif method == "tot_bfs":
+            agents = {
+                "step": AgentFactory.get("bfs", benchmark),
+                "evaluate": AgentFactory.get("evaluate", benchmark),
+            }
+        elif method == "got":
+            agents = {
+                "step": AgentFactory.get("act", benchmark),
+                "aggregate": AgentFactory.get("aggregate", benchmark),
+                "evaluate": AgentFactory.get("evaluate", benchmark),
+            }
+        elif method == "rap":
+            agents = {
+                "step": AgentFactory.get("react", benchmark),
+                "evaluate": AgentFactory.get("selfevaluate", benchmark),
+            }
+        elif method == "react":
+            agents = {
+                "step": AgentFactory.get("react", benchmark),
+            }
+        else:
+            raise NotImplementedError(f"Method {method} is not implemented yet.")
+        
+        # For the moment, only supporting same params for all agents
+        agents.update({k+"_params": params for k in agents.keys()})
+
         try:
-            return cls.registry[key](*args, **kwargs)
+            return cls.registry[key](agents=agents, *args, **kwargs)
         except KeyError:
             raise ValueError(f"No method found for name={method}")
