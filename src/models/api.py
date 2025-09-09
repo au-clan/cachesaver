7d22a775932df0d2559e3dbc095cbf855a234d7d
Potamitisn <nearchosp@outlook.com>
2025-09-09 10:47:51 +0200

    Adding tab functionality to API

--- src/models/api.py
+++ src/models/api.py
@@ -1,6 +1,8 @@
 import time
-from deepdiff import DeepHash
 from abc import ABC
+from deepdiff import DeepHash
+from collections import defaultdict
+
 from ..typedefs import Model, SingleRequestModel, DecodingParameters, Request
 from typing import List, Union
 
@@ -12,20 +14,22 @@ class API(ABC):
     def __init__(self, pipeline: Model, model: str):
         self.pipeline = pipeline
         self.model = model
-        self.calls = {
+        self.calls = defaultdict(lambda: {
             "total": 0,       # Total calls
             "cacher": 0,      # Calls saved by the cacher
             "deduplicator": 0 # Calls saved by the deduplicator
-            }
-        self.tokens = {
+            })
+        
+        self.tokens = defaultdict(lambda: {
             "total": {"in": 0, "out": 0},      # Total tokens
             "cacher": {"in": 0, "out": 0},     # Tokens saved by the cacher
             "duplicator": {"in": 0, "out": 0}, # Tokens saved by the deduplicator
-        }
-        self.latencies = []
-        self.reuse = {}
+        })
+
+        self.latencies = defaultdict(list)
+        self.reuse = defaultdict(dict)
     
-    async def request(self, prompt: Union[str, List[str]], n: int, request_id: str, namespace: str, params: DecodingParameters) -> List[str]:
+    async def request(self, prompt: Union[str, List[str]], n: int, request_id: str, namespace: str, params: DecodingParameters, tab: str="default") -> List[str]:
         """
         Send a request to the pipeline
         """
@@ -47,19 +51,19 @@ class API(ABC):
         end = time.perf_counter()
 
         # Measuring latency
-        self.latencies.append(end - start)
+        self.latencies[tab].append(end - start)
 
         # Measuring reuse
         hashed_prompt = DeepHash(prompt)[prompt]
         if hashed_prompt in self.reuse:
-            self.reuse[hashed_prompt] += n
+            self[tab].reuse[hashed_prompt] += n
         else:
-            self.reuse[hashed_prompt] = n
+            self[tab].reuse[hashed_prompt] = n
 
         # Measuring number of caslls
-        self.calls["total"] += len(response.data)
-        self.calls["cacher"] += sum(response.cached)
-        self.calls["deduplicator"] += sum(response.duplicated)
+        self[tab].calls["total"] += len(response.data)
+        self[tab].calls["cacher"] += sum(response.cached)
+        self[tab].calls["deduplicator"] += sum(response.duplicated)
 
         # Measuring number of tokens
         messages, tokin, tokout = zip(*response.data)
@@ -82,11 +86,11 @@ class API(ABC):
                 duplicated_in += in_tok
 
 
-        self.tokens["total"]["in"] += total_in
-        self.tokens["total"]["out"] += total_out
-        self.tokens["cacher"]["in"] += cached_in
-        self.tokens["cacher"]["out"] += cached_out
-        self.tokens["duplicator"]["in"] += duplicated_in
+        self[tab].tokens["total"]["in"] += total_in
+        self[tab].tokens["total"]["out"] += total_out
+        self[tab].tokens["cacher"]["in"] += cached_in
+        self[tab].tokens["cacher"]["out"] += cached_out
+        self[tab].tokens["duplicator"]["in"] += duplicated_in
 
         return messages
 
